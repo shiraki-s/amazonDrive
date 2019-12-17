b1c9e0ccec85ae894492a820fb1e36ef5a207ad5
shiraki <s-shiraki@tg-inet.co.jp>
2019-12-17 15:57:28 +0900

    更新

--- js/drive/image/getImageSrc.js
+++ js/drive/image/getImageSrc.js
@@ -7,18 +7,17 @@ function GetImageSrc(driveData) {
     this.init = function (callback) {
 
         driveImage.searchImages(FILTER, function (json) {
-            console.log(json);
-
             const array = [];
 
             for (let i = 0, len = json.data; i < len; i++) {
+
                 const name = json.data[i].name;
                 const src = json.data[i].originalUrl;
 
                 array.push({ name: name, src: src });
-
             }
 
+            console.log(array);
             driveData.setImageSrcs(array);
 
             callback();
